86ecc94ef839badea91e5d463df07392619102ba
Josh-Thompson2222 <118356604+Josh-Thompson2222@users.noreply.github.com>
2026-03-10 15:28:08 +0000

    Add files via upload

---
+++ AStar.cpp
@@ -0,0 +1,69 @@
+﻿/*
+*
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠿⠿⠿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⣉⣁⣤⣤⣶⣾⣿⣿⣶⡄⢲⣯⢍⠁⠄⢀⢹⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⢯⣾⣿⣿⣏⣉⣹⠿⠇⠄⠽⠿⢷⡈⠿⠇⣀⣻⣿⡿⣻
+⣿⣿⡿⠿⠛⠛⠛⢛⡃⢉⢣⡤⠤⢄⡶⠂⠄⠐⣀⠄⠄⠄⠄⠄⡦⣿⡿⠛⡇⣼
+⡿⢫⣤⣦⠄⠂⠄⠄⠄⠄⠄⠄⠄⠄⠠⠺⠿⠙⠋⠄⠄⠄⠢⢄⠄⢿⠇⠂⠧⣿
+⠁⠄⠈⠁⠄⢀⣀⣀⣀⣀⣠⣤⡤⠴⠖⠒⠄⠄⠄⠄⠄⠄⠄⠄⠄⠘⢠⡞⠄⣸
+⡀⠄⠄⠄⠄⠄⠤⠭⠦⠤⠤⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣂⣿
+⣷⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢳⠄⠄⢀⠈⣠⣤⣤⣼⣿
+⣿⣿⣷⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣴⣶⣶⣶⣄⡀⠄⠈⠑⢙⣡⣴⣿⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+
+Josh Thompson
+19/01/26
+*/
+
+#include <iostream>		//cout, endl
+#include <cmath>
+#include "AStar.h"	//HelloWorld()
+#include <cstdlib>
+#include <ctime>
+
+void AStar::PrintAStar()
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            if (i == startY && j == startX)
+                std::cout << "S "; // Start
+            else if (i == endY && j == endX)
+                std::cout << "E "; // End
+            else if (grid[i][j] == 1)
+                std::cout << "# "; // Wall
+            else
+                std::cout << ". "; // Walkable
+        }
+        std::cout << std::endl;
+    }
+}
+
+AStar::AStar(int r, int c)
+{
+    rows = r;
+    cols = c;
+
+    grid.resize(rows, std::vector<int>(cols, 0));
+
+    std::srand(std::time(nullptr));  // Seed once
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            grid[i][j] = std::rand() % 5;  // 20% of the entire grid is a wall. The other 80% is walkable.
+        }
+    }
+
+    startX = 3;
+    startY = 1;
+
+    endX = 7;
+    endY = 8;
+
+    grid[startX][startY] = 0; // Ensure start is walkable
+    grid[endX][endY] = 0; // Ensure end is walkable
+}
